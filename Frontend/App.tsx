c322dce2a2d0145056c460c3c2b4e2e40596a54e
Sebastian Prado <147444585+Gamer-Sm@users.noreply.github.com>
2025-08-21 14:06:58 -0500

    Add files via upload

--- Frontend/App.tsx
+++ Frontend/App.tsx
@@ -2,34 +2,45 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import FlashMessage from 'react-native-flash-message'; // 🔑 import
+
+// Screens
 import LoginScreen from './src/screens/LoginScreen';
 import SignupScreen from './src/screens/SignupScreen';
+import RequestCodeScreen from './src/screens/request-code';
+import RequestPasswordScreen from './src/screens/request-password';
 
 // Definimos los nombres de las pantallas y sus parámetros
 export type RootStackParamList = {
   Login: undefined;
   Signup: undefined;
+  "request-password": { email: string };
+  "request-code": { email: string };
 };
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName="Login"
-        screenOptions={{
-          headerShown: false,
-          animation: 'slide_from_right',   // transición lateral
-          animationDuration: 400,         // duración de la animación
-        }}
-      >
-        {/* Pantalla de login */}
-        <Stack.Screen name="Login" component={LoginScreen} />
+    <>
+      <NavigationContainer>
+        <Stack.Navigator
+          initialRouteName="Login"
+          screenOptions={{
+            headerShown: false,
+            animation: 'slide_from_right',
+          }}
+        >
+          <Stack.Screen name="Login" component={LoginScreen} />
+          <Stack.Screen name="Signup" component={SignupScreen} />
+          <Stack.Screen name="request-code" component={RequestCodeScreen} />
+          <Stack.Screen name="request-password" component={RequestPasswordScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
 
-        {/* Pantalla de registro */}
-        <Stack.Screen name="Signup" component={SignupScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+      {/* 🔑 FlashMessage debe estar afuera del NavigationContainer */}
+      <FlashMessage position="top" />
+    </>
   );
 }
+ 
